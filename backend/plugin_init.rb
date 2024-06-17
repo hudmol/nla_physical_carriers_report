f8d001c220e390c49a5a90d8293b3719917e514b
James Bullen <jambun@gmail.com>
2024-06-17 13:45:10 +1000

    changed 'moving images' to 'moving image'

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,7 +1,7 @@
 # check config for subjects to match on
-# default: ['born digital', 'audio', 'moving images']
+# default: ['born digital', 'audio', 'moving image']
 unless AppConfig.has_key?(:nla_physical_carriers_report_subjects)
-  AppConfig[:nla_physical_carriers_report_subjects] = ['born digital', 'audio', 'moving images']
+  AppConfig[:nla_physical_carriers_report_subjects] = ['born digital', 'audio', 'moving image']
 end
 
 subjects = AppConfig[:nla_physical_carriers_report_subjects]
@@ -14,7 +14,7 @@ subjects.each do |subject|
 end
 
 if subjects.empty?
-  Log.error("ERROR: nla_physical_carriers_report plugin is badly configured! It has no subjects. Use AppConfig[:nla_physical_carriers_report_subjects] to set an array of subjects, or don't set it to accept the default: ['born digital', 'audio', 'moving images']")
+  Log.error("ERROR: nla_physical_carriers_report plugin is badly configured! It has no subjects. Use AppConfig[:nla_physical_carriers_report_subjects] to set an array of subjects, or don't set it to accept the default: ['born digital', 'audio', 'moving image']")
 elsif nonexistent_subjects.empty?
   Log.info("nla_physical_carriers_report plugin is correctly configured with subjects: #{subjects.join(', ')}")
 else
--- frontend/plugin_init.rb
+++ frontend/plugin_init.rb
@@ -1,4 +1,4 @@
-# default: ['born digital', 'audio', 'moving images']
+# default: ['born digital', 'audio', 'moving image']
 unless AppConfig.has_key?(:nla_physical_carriers_report_subjects)
-  AppConfig[:nla_physical_carriers_report_subjects] = ['born digital', 'audio', 'moving images']
+  AppConfig[:nla_physical_carriers_report_subjects] = ['born digital', 'audio', 'moving image']
 end
